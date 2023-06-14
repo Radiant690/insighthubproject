ad6c8b7cd72ba43d1cb0ba23b77c81f01b451914
venkatasai <venkatasaikadamati@gmail.com>
2023-06-14 12:59:02 +0530

    Configured ./functions/index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,36 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+admin.initializeApp();
+const db = admin.firestore();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const createUserDocument = functions.auth
+  .user()
+  .onCreate(async (user) => {
+    db.collection("users")
+      .doc(user.uid)
+      .set(JSON.parse(JSON.stringify(user)));
+  });
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const deletePostComments = functions.firestore
+  .document(`posts/{postId}`)
+  .onDelete(async (snap) => {
+    const postId = snap.id;
+    console.log("HERE IS POST ID", postId);
+
+    admin
+      .firestore()
+      .collection("comments")
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          if (doc.data().postId === postId) {
+            console.log("DELETING COMMENT: ", doc.id, doc.data().text);
+            doc.ref.delete();
+          }
+        });
+      })
+      .catch((error) => {
+        console.log("Error deleting post comments");
+      });
+  });
